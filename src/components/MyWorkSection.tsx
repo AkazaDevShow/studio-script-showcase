abffb3315a7062d2199a6cc27e9e7816231b72c2
AkazaDev <130745198+AkazaDevShow@users.noreply.github.com>
2025-07-26 20:31:30 +0300

    MyWorkSection.tsx

--- src/components/MyWorkSection.tsx
+++ src/components/MyWorkSection.tsx
@@ -15,6 +15,12 @@ const MyWorkSection = () => {
   ];
 
   const workContent = {
+        server: {
+      title: '',
+      description: '',
+            features: [
+      ]
+    },
     client: {
       title: 'Client-Side Scripts',
       description: 'Responsive UI systems and local player experiences that enhance gameplay.',
